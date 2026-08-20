eca99100b45f1384e454e5a39efa8fe852694b62
DAVI FARIAS MADRUGA <davi.madruga@cs.unipe.edu.br>
2026-08-20 11:26:25 -0300

    Feat: class Diretor

--- Exercicio02/src/main/java/Diretor.java
+++ Exercicio02/src/main/java/Diretor.java
@@ -1,2 +1,13 @@
-public class Diretor {
+public class Diretor extends Gerente{
+    private double participacaoLucros;
+
+    public Diretor(String nome, String matricula, double salarioBase, String dataAdmissao, double valeTransporte, double valeAlimentacao, int tamanhoEquipe, int percentualBonus, double participacaoLucros) {
+        super(nome, matricula, salarioBase, dataAdmissao, valeTransporte, valeAlimentacao, tamanhoEquipe, percentualBonus);
+        this.participacaoLucros = participacaoLucros;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + participacaoLucros;
+    }
 }
