641c63d30660f38f1f87e131c6ad6fc10896deb5
DAVI FARIAS MADRUGA <davi.madruga@cs.unipe.edu.br>
2026-08-20 10:41:56 -0300

    Classe veiculo

---
+++ Exemplo03/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Veiculo veiculo = new Moto("Corolla", 2010, 125);
+
+        System.out.println(veiculo.imprimirDetalhes());
+    }
+}
---
+++ Exemplo03/src/main/java/Veiculo.java
@@ -0,0 +1,15 @@
+public class Veiculo {
+
+    protected String modelo;
+    protected int ano;
+
+    public Veiculo(String modelo, int ano){
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String imprimirDetalhes(){
+        return "\nModelo: "+ this.modelo + "\nAno: " + this.ano;
+    }
+
+}
